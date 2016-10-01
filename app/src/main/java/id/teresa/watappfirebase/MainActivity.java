dc6529890e2c6d9d93ddd9c908feb39c4a9d3f1b
pnteresa <teresapranyoto@gmail.com>
2016-10-01 09:47:29 +0700

    Added Message class, adapter for recyclerview, dummy data

--- app/src/main/java/id/teresa/watappfirebase/MainActivity.java
+++ app/src/main/java/id/teresa/watappfirebase/MainActivity.java
@@ -2,12 +2,41 @@ package id.teresa.watappfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<Message> messageList = new ArrayList<Message>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MessageAdapter(getBaseContext(), messageList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        addDummyMessages();
+    }
+
+    private void addDummyMessages() {
+        messageList.add(new Message("Knock knock", Message.Sender.ME));
+        messageList.add(new Message("Who's there?", Message.Sender.OTHER));
+        messageList.add(new Message("Harry", Message.Sender.ME));
+        messageList.add(new Message("Harry who?", Message.Sender.OTHER));
+        messageList.add(new Message("Harry up it's cold up here!", Message.Sender.ME));
     }
 }
